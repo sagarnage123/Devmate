b6aa4ed9236da8fd9411e7a7fd1566b53426f8b1
sagarnage884 <sagernage1@gmail.com>
2025-07-04 23:32:30 +0530

    Initial commit:Setup express backend with MongoDb and .gitignore

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const cors=require("cors");
+const mongoose=require("mongoose");
+require("dotenv").config();
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+
+    res.send("Hello");
+
+});
+
+app.get("/api/test",(req,res)=>{
+    res.json({message:"Hey hello from devmate"});
+
+});
+
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log("✅ MongoDB connected");  
+}).catch((e)=>{
+    console.log('❌ MongoDB connection failed:', e);
+    
+});
+
+app.listen(PORT,(err)=>{
+
+    if(err)
+    {
+        console.log('Error found while setting up the server',err);
+    }
+    else
+    console.log(` Server is running at the PORT ${PORT}`);
+    
+});
