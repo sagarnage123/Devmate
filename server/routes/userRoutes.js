e15157e35d786eaa86b39e7cf17f7dec52a875d3
sagarnage884 <sagernage1@gmail.com>
2025-07-07 23:23:02 +0530

    Middlewarw is created for authentication of JWT

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express=require("express");
 const router=express.Router();
 const {registerUser,loginUser}=require("../controllers/userController.js");
+const {protect}=require("../middleware/authMiddleware.js");
+const User = require("../models/User.js");
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
+
+router.get("/profile",protect,(req,res)=>{
+    res.json(req.user);
+})
 
 module.exports=router;
