fad43f8474e764e0aaf93c0b33c9b1d414c63504
sagarnage123 <sagernage1@gmail.com>
2025-10-01 09:57:36 +0530

    Issue related to tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports={
+  content: [
+    "./frontend/index.html",          // your HTML file
+    "./frontend/src/**/*.{js,jsx}",   // all React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
