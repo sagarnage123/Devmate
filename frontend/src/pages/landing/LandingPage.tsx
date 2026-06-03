1daf250b42dc6b33e8f72c998b2754b2042111e2
sagarnage123 <sagernage1@gmail.com>
2026-06-03 16:10:50 +0530

    Added landing page

---
+++ frontend/src/pages/landing/LandingPage.tsx
@@ -0,0 +1,22 @@
+import Hero from "@/components/landing/Hero";
+import ProblemSolution from "@/components/landing/ProblemSolution";
+import Workflow from "@/components/landing/Workflow";
+import Architecture from "@/components/landing/Architecture";
+import EngineeringChallenges from "@/components/landing/EngineeringChallenges";
+import Security from "@/components/landing/Security";
+import ProductWalkthrough from "@/components/landing/ProductWalkthrough";
+
+export default function LandingPage() {
+    return (
+        <main className="min-h-screen bg-indigo-950 text-white">
+            <Hero />
+            <ProblemSolution/>
+            <Workflow/>
+            <Architecture />
+            <EngineeringChallenges />
+            <Security />
+            <ProductWalkthrough />
+            
+        </main>
+    );
+}
